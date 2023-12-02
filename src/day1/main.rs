e4c2f241e27fef122ab145a3e06ec317a3f1bf87
blueStruct <mdiewald@protonmail.com>
2023-12-02 13:57:11 +0000

    day1 part1 solved

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use core::fmt::Write;
+use std::{fs, error::Error};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input: String = fs::read_to_string("src/day1/input")?.parse()?;
+
+    let calibration_values: Vec<u32> = input.lines().map(|line| -> u32 {
+        let first_digit = line
+            .chars()
+            .skip_while(|x| !x.is_digit(10))
+            .nth(0)
+            .expect("Could not find first digit");
+        let last_digit = line
+            .chars()
+            .rev()
+            .skip_while(|x| !x.is_digit(10))
+            .nth(0)
+            .expect("Could not find last digit");
+        let mut number = String::new();
+        let _ = write!(number, "{}{}", first_digit, last_digit);
+        number.parse::<u32>().unwrap()
+    }).collect();
+
+    let sum: u32 = calibration_values.iter().sum();
+    println!("{}", sum);
+
+    Ok(())
 }

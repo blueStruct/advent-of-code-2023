22c21e569158dd3d341e04395e6482a048daecbc
blueStruct <mdiewald@protonmail.com>
2023-12-02 20:29:11 +0000

    template -> day3

---
+++ src/day3/main.rs
@@ -0,0 +1,19 @@
+use std::{error::Error, fs};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input: String = fs::read_to_string("src/day3/input")?.parse()?;
+    part1(&input);
+    part2(&input);
+
+    Ok(())
+}
+
+
+fn part1(input: &str) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+
+fn part2(input: &str) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}

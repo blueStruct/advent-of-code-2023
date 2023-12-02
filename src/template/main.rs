479db85cb1a0dbed0af51fbd520373eaab73d4d7
blueStruct <mdiewald@protonmail.com>
2023-12-02 20:09:46 +0000

    improved template

--- src/template/main.rs
+++ src/template/main.rs
@@ -2,5 +2,18 @@ use std::{error::Error, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input: String = fs::read_to_string("src/day1/input")?.parse()?;
+    part1(&input);
+    part2(&input);
+
+    Ok(())
+}
+
+
+fn part1(input: &str) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+
+fn part2(input: &str) -> Result<(), Box<dyn Error>> {
     Ok(())
 }

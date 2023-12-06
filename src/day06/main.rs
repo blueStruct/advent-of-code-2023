6f5ba2110757bfeed5c172afef3026797f4c1aff
blueStruct <mdiewald@protonmail.com>
2023-12-06 15:49:25 +0000

    refactoring

--- src/day06/main.rs
+++ src/day06/main.rs
@@ -34,29 +34,26 @@ fn part1(input: &str) -> Result<(), Box<dyn Error>> {
 
     let mut results = vec![];
 
-    times
-        .iter()
-        .zip(distances)
-        .for_each(|(&available_time, min_distance)| {
-            let mut count = 0u32;
-            let mut success_zone = false;
+    for (&available_time, min_distance) in times.iter().zip(distances) {
+        let mut count = 0u32;
+        let mut success_zone = false;
 
-            for t_button_down in 1..available_time {
-                let distance = t_button_down * (available_time - t_button_down);
+        for t_button_down in 1..available_time {
+            let distance = t_button_down * (available_time - t_button_down);
 
-                if distance > min_distance {
-                    count += 1;
-                    success_zone = true;
-                } else {
-                    // stop iterating, not working any more
-                    if success_zone == true {
-                        break;
-                    }
+            if distance > min_distance {
+                count += 1;
+                success_zone = true;
+            } else {
+                // stop iterating, not working any more
+                if success_zone == true {
+                    break;
                 }
             }
+        }
 
-            results.push(count);
-        });
+        results.push(count);
+    }
 
     let product: u32 = results.iter().product();
 

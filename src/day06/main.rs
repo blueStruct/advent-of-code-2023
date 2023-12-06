a068b642d1b702ea16d807a6866c8428a458224f
blueStruct <mdiewald@protonmail.com>
2023-12-06 15:46:12 +0000

    day6 part2 solved

--- src/day06/main.rs
+++ src/day06/main.rs
@@ -1,5 +1,5 @@
-use std::{error::Error, fs};
 use regex::Regex;
+use std::{error::Error, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input: String = fs::read_to_string("src/day06/input")?.parse()?;
@@ -8,7 +8,6 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
-
 
 fn part1(input: &str) -> Result<(), Box<dyn Error>> {
     let times: Vec<u64> = Regex::new(r"Time:(.*)\n")
@@ -35,26 +34,29 @@ fn part1(input: &str) -> Result<(), Box<dyn Error>> {
 
     let mut results = vec![];
 
-    times.iter().zip(distances).for_each(|(&available_time, min_distance)| {
-        let mut count = 0u32;
-        let mut success_zone = false;
+    times
+        .iter()
+        .zip(distances)
+        .for_each(|(&available_time, min_distance)| {
+            let mut count = 0u32;
+            let mut success_zone = false;
 
-        for t_button_down in 1..available_time {
-            let distance = t_button_down * (available_time - t_button_down);
+            for t_button_down in 1..available_time {
+                let distance = t_button_down * (available_time - t_button_down);
 
-            if distance > min_distance {
-                count += 1;
-                success_zone = true;
-            } else {
-                // stop iterating, not working any more
-                if success_zone == true {
-                    break;
+                if distance > min_distance {
+                    count += 1;
+                    success_zone = true;
+                } else {
+                    // stop iterating, not working any more
+                    if success_zone == true {
+                        break;
+                    }
                 }
             }
-        } 
 
-        results.push(count);
-    });
+            results.push(count);
+        });
 
     let product: u32 = results.iter().product();
 
@@ -63,7 +65,51 @@ fn part1(input: &str) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-
 fn part2(input: &str) -> Result<(), Box<dyn Error>> {
+    let available_time: u64 = Regex::new(r"Time:(.*)\n")
+        .unwrap()
+        .captures(input)
+        .unwrap()
+        .get(1) // index 1 is the capture group in the regex
+        .unwrap()
+        .as_str()
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .join("")
+        .parse()
+        .unwrap();
+
+    let min_distance: u64 = Regex::new(r"Distance:(.*)\z")
+        .unwrap()
+        .captures(input)
+        .unwrap()
+        .get(1) // index 1 is the capture group in the regex
+        .unwrap()
+        .as_str()
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .join("")
+        .parse()
+        .unwrap();
+
+    let mut count = 0u32;
+    let mut success_zone = false;
+
+    for t_button_down in 1..available_time {
+        let distance = t_button_down * (available_time - t_button_down);
+
+        if distance > min_distance {
+            count += 1;
+            success_zone = true;
+        } else {
+            // stop iterating, not working any more
+            if success_zone == true {
+                break;
+            }
+        }
+    }
+
+    println!("The answer for part 2 is: {}", count);
+
     Ok(())
 }
